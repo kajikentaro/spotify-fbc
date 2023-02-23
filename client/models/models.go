010eb100ac93bea5a38a07738a00bd211eff572b
kajikentaro <kajindowsxp@gmail.com>
2023-02-24 00:59:41 +0900

    Add: ファイルの中に楽曲情報を書き込むまで

--- client/models/models.go
+++ client/models/models.go
@@ -2,7 +2,11 @@ package models
 
 import (
 	"context"
-	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/zmb3/spotify/v2"
 )
@@ -18,12 +22,10 @@ func NewModel(client *spotify.Client, ctx context.Context) model {
 
 func (m *model) CreatePlaylistDirectory(playlist spotify.SimplePlaylist) error {
 	// TODO: playlist.Name に /が含まれる場合を除く
-	/*
-		err := os.Mkdir(playlist.Name, os.ModePerm)
-		if os.IsExist(err) {
-			log.Println(playlist.Name, "is already created")
-		}
-	*/
+	err := os.Mkdir(playlist.Name, os.ModePerm)
+	if os.IsExist(err) {
+		log.Println(playlist.Name, "is already created")
+	}
 
 	playlistItemPage, err := m.client.GetPlaylistItems(m.ctx, playlist.ID)
 	if err != nil {
@@ -32,10 +34,36 @@ func (m *model) CreatePlaylistDirectory(playlist spotify.SimplePlaylist) error {
 	for _, playlistItem := range playlistItemPage.Items {
 		track := playlistItem.Track.Track
 		// TODO: track.Name に /が含まれる場合を除く
-		// ioutil.WriteFile(playlist.Name+"/"+track.Name+".txt", []byte(fmt.Sprintf("%v\n", track)), 0666)
-		fmt.Println(track.Name)
+		textContent := getTextContent(track)
+		ioutil.WriteFile(playlist.Name+"/"+track.Name+".txt", []byte(textContent), 0666)
 	}
 	return nil
+}
+
+func getTextContent(track *spotify.FullTrack) string {
+	dd := [][]string{
+		{"id", track.ID.String()},
+		{"name", track.Name},
+		{"artist", joinArtistText(track.Artists)},
+		{"album", track.Album.Name},
+		{"seconds", strconv.Itoa(track.Duration)},
+		{"isrc", track.ExternalIDs["isrc"]},
+	}
+
+	text := ""
+	for _, d := range dd {
+		text += d[0] + " " + d[1] + "\n"
+	}
+
+	return text
+}
+
+func joinArtistText(artists []spotify.SimpleArtist) string {
+	text := []string{}
+	for _, a := range artists {
+		text = append(text, a.Name)
+	}
+	return strings.Join(text, ", ")
 }
 
 func (m *model) PullPlaylists() error {
@@ -43,7 +71,7 @@ func (m *model) PullPlaylists() error {
 	if err != nil {
 		return err
 	}
-	for _, v := range playlists.Playlists {
+	for _, v := range playlists.Playlists[:1] {
 		err := m.CreatePlaylistDirectory(v)
 		if err != nil {
 			return err
