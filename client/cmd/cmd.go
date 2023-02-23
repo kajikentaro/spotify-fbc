7f18e3ab6f7aed00d9cda3505415e041b88cce3a
kajikentaro <kajindowsxp@gmail.com>
2023-02-23 23:59:53 +0900

    Add: pullコマンドで動くように

--- client/cmd/cmd.go
+++ client/cmd/cmd.go
@@ -2,28 +2,74 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/kajikentaro/spotify-file-based-client/client/logins"
 	"github.com/kajikentaro/spotify-file-based-client/client/models"
+	"github.com/spf13/cobra"
+	"github.com/zmb3/spotify/v2"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
 )
 
-var redirectURI = "http://localhost:8080/callback"
+var rootCmd = &cobra.Command{
+	Use:   "spotifyfbc",
+	Short: "spotifyfbc: Spotify file-based client",
+	Long: `Spotify file-based client
+  Edit your playlists by moving directories and file locations`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 func init() {
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(pullCmd)
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// rand.Seed(time.Now().UnixNano())
 }
 
-func Execute() {
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of spotifyfbc",
+	Long:  `All software has versions. This is spotifyfilefbc's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("0.0.1")
+	},
+}
+
+var pullCmd = &cobra.Command{
+	Use:   "pull",
+	Short: "TODO",
+	Long:  `TODO`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		client := genClient(ctx)
+
+		/* generate model */
+		model := models.NewModel(client, ctx)
+
+		/* use API via model*/
+		model.PullPlaylists()
+	},
+}
+
+func genClient(ctx context.Context) *spotify.Client {
+	var redirectURI = "http://localhost:8080/callback"
 	/* set up variables */
 	clientID := os.Getenv("CLIENT_ID")
 	clientSecret := os.Getenv("CLIENT_SECRET")
@@ -33,7 +79,6 @@ func Execute() {
 		spotifyauth.WithClientID(clientID),
 		spotifyauth.WithClientSecret(clientSecret),
 	)
-	ctx := context.Background()
 
 	login := logins.NewLogin(ctx, auth)
 
@@ -61,10 +106,5 @@ func Execute() {
 		}
 	}
 	client := login.GetClient(token)
-
-	/* generate model */
-	model := models.NewModel(client, ctx)
-
-	/* use API via model*/
-	model.PullPlaylists()
+	return client
 }
--- client/go.mod
+++ client/go.mod
@@ -4,13 +4,15 @@ go 1.19
 
 require (
 	github.com/joho/godotenv v1.5.1
-	github.com/zmb3/spotify v1.3.0
+	github.com/spf13/cobra v1.6.1
 	github.com/zmb3/spotify/v2 v2.3.1
 	golang.org/x/oauth2 v0.5.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.6.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
